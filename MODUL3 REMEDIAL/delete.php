de81be9ae16637283dc3fc0e6cd797cbc4947dac
LANG <115536495+ERLANGGA180203@users.noreply.github.com>
2023-01-04 16:32:19 +0700

    Delete MODUL3 REMEDIAL directory

--- MODUL3 REMEDIAL/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include('connect.php');
-
-    $id = $_GET['id_mobil'];
-    $query = "DELETE FROM showroom_erlangga_table WHERE id_mobil = $id";
-    $delete = mysqli_query($connect, $query);
-    
-    header('Location: MY_ITEM.php');
-?>
